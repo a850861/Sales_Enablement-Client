38f28b61d7857bd88377f29c3ac27dee87810f7a
a850861 <chetan.singh@yara.com>
2024-05-02 14:25:51 +0530

    added sidebar of screenone

--- src/Components/ScreenOne/FirstPage.js
+++ src/Components/ScreenOne/FirstPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Topbar from "./Topbar";
+import Sidebar from "./Sidebar";
 
 const FirstPage = () => {
   return (
     <div>
       <Topbar />
+      <Sidebar />
     </div>
   );
 };
